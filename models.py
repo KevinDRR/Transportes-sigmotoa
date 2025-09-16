adc2752f04729637a903ad750f186272007fd033
sigmotoa <si.galvismotoa@gmail.com>
2025-09-16 10:37:41 -0500

    Setting up without issues

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+
 from utils import Kind
 
 
-class PetBase(BaseModel):
-    name: str
-    year: int
-    kind: Kind
+class PetBase(SQLModel):
+    name: str  | None = Field(description="Pet name")
+    year: int  | None= Field(description="Pet year")
+    kind: Kind  | None = Field(description="Pet kind", default=Kind.Dog)
 
-class Pet(PetBase):
-    id: int
+class Pet(PetBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
 
 class PetCreate(PetBase):
     pass
