9bd7728d43732fb22de54e8ba16558a402c309e9
sigmotoa <si.galvismotoa@gmail.com>
2025-09-23 10:40:44 -0500

    Endpoints with business logic

--- pet.py
+++ pet.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from models import Pet, PetCreate
+from fastapi import APIRouter, HTTPException
+from models import Pet, PetCreate, User
 from db import SessionDep
 
 
@@ -7,7 +7,11 @@ router = APIRouter()
 
 @router.post("/", response_model=Pet)
 async def create_pet(new_pet: PetCreate, session: SessionDep):
-    pet = Pet.model_validate(new_pet)
+    pet_data = new_pet.model_dump()
+    user_db=session.get_one(User,pet_data.get("user_id"))
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    pet = Pet.model_validate(pet_data)
     session.add(pet)
     session.commit()
     session.refresh(pet)
@@ -17,3 +21,10 @@ async def create_pet(new_pet: PetCreate, session: SessionDep):
 @router.get("/", response_model=list[Pet], summary="Get all pets from the DB")
 async def all_pets(session: SessionDep):
     return session.query(Pet).all()
+
+@router.get("/{pet_id}", response_model=Pet)
+async def get_one_pet(pet_id: int, session: SessionDep):
+    pet_db = session.get_one(Pet, pet_id)
+    if not pet_db:
+        raise HTTPException(status_code=404, detail="Pet not found")
+    return pet_db
