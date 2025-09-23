d713303345360f288c3200ce42d533c99e9c8616
sigmotoa <si.galvismotoa@gmail.com>
2025-09-23 10:40:29 -0500

    Create a relation between user(1) - pet(n)

--- models.py
+++ models.py
@@ -1,28 +1,37 @@
 from pydantic import BaseModel
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from utils import Kind
 
-class PetBase(SQLModel):
-    name: str  | None = Field(description="Pet name")
-    year: int  | None= Field(description="Pet year")
-    kind: Kind  | None = Field(description="Pet kind", default=Kind.Dog)
-
-class Pet(PetBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-
-class PetCreate(PetBase):
-    pass
-
-class PetUpdate(PetBase):
-    pass
 
 class UserBase(SQLModel):
     name: str | None = Field(description="User name")
     year: int | None = Field(description="User year")
     status: bool | None = Field(description="User status", default=True)
 
+
 class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    pets: list["Pet"] = Relationship(back_populates="user")
 
 class UserCreate(UserBase):
     pass
+
+
+class PetBase(SQLModel):
+    name: str  | None = Field(description="Pet name")
+    year: int  | None= Field(description="Pet year")
+    kind: Kind  | None = Field(description="Pet kind", default=Kind.Dog)
+    alive: bool | None = Field(description="Pet alive", default=True)
+
+class Pet(PetBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_id:int =Field(foreign_key="user.id")
+    user: User = Relationship(back_populates="pets")
+
+class PetCreate(PetBase):
+    user_id:int =Field(foreign_key="user.id")
+
+
+class PetUpdate(PetBase):
+    pass
+
