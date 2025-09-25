83337494c777729feba75aea9e8299ac26f9b87a
sigmotoa <si.galvismotoa@gmail.com>
2025-09-25 07:49:01 -0500

    Adding a appoinment model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+import datetime
 from sqlmodel import SQLModel, Field, Relationship
 from utils import Kind
 
@@ -28,10 +28,27 @@ class Pet(PetBase, table=True):
     user_id:int =Field(foreign_key="user.id")
     user: User = Relationship(back_populates="pets")
 
+    vets: list["Vet"] = Relationship(back_populates="pets", link_model="Appointment")
+
 class PetCreate(PetBase):
     user_id:int =Field(foreign_key="user.id")
 
 
 class PetUpdate(PetBase):
     pass
+
+class VetBase(SQLModel):
+    name: str | None = Field(description="Vet name")
+
+class Vet(VetBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    pets: list["Pet"] = Relationship(back_populates="vets", link_model="Appointment")
+
+
+class Appointment(SQLModel, table=True):
+    pet_id: int | None = Field(default=None, foreign_key="pet.id", primary_key=True)
+    vet_id: int | None = Field(default=None, foreign_key="vet.id", primary_key=True)
+    date: datetime.datetime | None = Field(default=datetime.datetime.now())
+
+
 
